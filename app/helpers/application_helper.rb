bfa5d827e722c95a054e7333163060a319ca17e9
Iggy <igoririanto@rocketmail.com>
2017-09-27 16:34:40 -0700

    figuring out how to update availability_array

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def show_available_hour(model)
     return model.availability.group_by(&:available_on)
   end
+
+  def say_hello
+    return "hello"
+  end
+
+  def user_availability_hours
+    obj = {}
+    24.times {|el| obj[el] = "null"}
+
+    return obj
+  end
 end
