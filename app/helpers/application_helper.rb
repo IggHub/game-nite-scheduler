cd22f3b62c6b06213f0e2108e4790bad251b67b5
Iggy <igoririanto@rocketmail.com>
2017-09-27 17:52:40 -0700

    changed the block color, but it is still unknown where the date is from

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  @green = "#36D7B7"
+  @gray = "#B7BABB"
+  @red = "#EC644B"
+
   def show_available_hour(model)
     return model.availability.group_by(&:available_on)
   end
@@ -7,10 +11,31 @@ module ApplicationHelper
     return "hello"
   end
 
-  def user_availability_hours
+  def availability_hours_object(arr)
     obj = {}
     24.times {|el| obj[el] = "null"}
+    arr.each {|el| obj[el] = true}
 
     return obj
   end
+
+  def get_user_availability_object(user)
+    return availability_hours_object(user.availability.where(available_on: Date.today - 2).map{|el| el.available_hour})
+  end
+
+  def get_user_availability_array(user)
+    return user.availability.where(available_on: Date.today).map{|el| el.available_hour}
+  end
+
+  def which_color(availability_bool)
+    case availability_bool
+    when true
+      return "background:#36D7B7"
+    when false
+      return "background:#EC644B"
+    else
+      return "background:"#B7BABB"
+    end
+  end
+
 end
