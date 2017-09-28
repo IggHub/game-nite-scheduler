6a81306ebce053a141c262a189f4fb496c97ee23
Iggy <igoririanto@rocketmail.com>
2017-09-28 13:12:27 -0700

    fix ordering on schedule

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @users = User.all_except(current_user).order(:name)
+    @friends = current_user.friends.order(:name)
     @availabilities = @user.availability.group_by(&:available_on)
 
     respond_to do |format|
